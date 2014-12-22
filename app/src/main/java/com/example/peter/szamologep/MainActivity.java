019cd0d79a22172813fd8643e37bd3383774c018
Peter Gnadig <peter.gnadig@gmail.com>
2014-12-22 18:01:48 +0100

    .

--- app/src/main/java/com/example/peter/szamologep/MainActivity.java
+++ app/src/main/java/com/example/peter/szamologep/MainActivity.java
@@ -4,6 +4,10 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Button;
+
+
 
 
 public class MainActivity extends ActionBarActivity {
